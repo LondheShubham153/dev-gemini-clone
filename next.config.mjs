128e1ba431455c053a756549ced08aea360c05c9
Amitabh-DevOps <amitabhdevops2024@gmail.com>
2025-03-21 21:55:14 +0530

    updated next config

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,11 @@ const nextConfig = {
         hostname: "lh3.googleusercontent.com",
         pathname: '/**',  // Wildcard for all paths under this hostname
       },
+      {
+        protocol: "https",
+        hostname: "i.ytimg.com",
+        pathname: '/**',  // Wildcard for all paths under this hostname
+      },
     ],
     unoptimized: true,  // Disables image optimization for this pattern
   },
