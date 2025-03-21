0687b53a228b1cc87c4cc649dbf5beb82567f312
Amitabh-DevOps <amitabhdevops2024@gmail.com>
2025-03-21 16:11:43 +0530

    Changes in code

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    metadataBase: new URL("http://34.247.167.192:3000"), // Replace with your actual domain/IP
+    metadataBase: new URL("https://gemini.letsdeployit.com"), // Replace with your actual domain/IP
   },
   images: {
     remotePatterns: [
