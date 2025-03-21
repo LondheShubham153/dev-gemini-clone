ff42eee3c2ad4cdc104c7f090add86b2f4835413
Amitabh-DevOps <amitabhdevops2024@gmail.com>
2025-03-21 20:47:01 +0530

    updated next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    metadataBase: new URL("https://gemini.letsdeployit.com"), // Replace with your actual domain/IP
-  },
   images: {
     remotePatterns: [
       {
         protocol: "https",
         hostname: "lh3.googleusercontent.com",
-        port: "",
       },
     ],
   },
 };
 
-export default nextConfig;
