a8da30431356f1c7f46e536bcf0906a9a6c5e94d
Amitabh-DevOps <amitabhdevops2024@gmail.com>
2025-03-23 12:02:19 +0530

    Updated page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ const page = async() => {
   if(session) redirect("/app")
   return (
     <section className='md:bg-[url("/assets/gemini-banner.png")] bg-[url("/assets/gemini-phone-banner.png")] bg-cover bg-no-repeat bg-center h-full w-full'>
-      <DevButton target='_blank' href='https://github.com/devyanshyadav/dev-gemini-clone' variant='v3' size='lg' asIcon rounded='full' className="text-4xl fixed top-4 right-4 text-white">
+      <DevButton target='_blank' href='https://github.com/Amitabh-DevOps/dev-gemini-clone' variant='v3' size='lg' asIcon rounded='full' className="text-4xl fixed top-4 right-4 text-white">
         <FaGithub />
       </DevButton>
       <DevButton href='/app' size='lg' rounded='full' className="px-10 !text-white group text-xl !bg-accentBlue/70 fixed gap-3 -translate-x-1/2  left-1/2 bottom-10">
