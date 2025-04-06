d85f40468dbc0453ee41f79956630b60ee244c9b
Amitabh-DevOps <amitabhdevops2024@gmail.com>
2025-04-06 14:19:55 +0530

    Added public ip display in terraform and inbound rule added

--- Terraform-EKS-Deployment/eks.tf
+++ Terraform-EKS-Deployment/eks.tf
@@ -19,7 +19,7 @@ module "eks" {
   }
 
   vpc_id                   = module.vpc.vpc_id
-  subnet_ids               = module.vpc.private_subnets
+  subnet_ids               = module.vpc.public_subnets
   control_plane_subnet_ids = module.vpc.intra_subnets
 
   # EKS Managed Node Group(s)
@@ -34,7 +34,6 @@ module "eks" {
 
 
   eks_managed_node_groups = {
-
     gemini-ng = {
       min_size     = 2
       max_size     = 3
@@ -43,12 +42,49 @@ module "eks" {
       instance_types = ["t2.medium"]
       capacity_type  = "SPOT"
 
+      associate_public_ip_address = true
+
       tags = {
         ExtraTag = "geminiapp"
       }
     }
   }
 
+
+  node_security_group_additional_rules = {
+    allow_nodeport_range = {
+      type                     = "ingress"
+      from_port                = 30000
+      to_port                  = 32767
+      protocol                 = "tcp"
+      description              = "Allow NodePort range"
+      cidr_blocks              = ["0.0.0.0/0"]
+    }
+    allow_port_9000 = {
+      type                     = "ingress"
+      from_port                = 9000
+      to_port                  = 9000
+      protocol                 = "tcp"
+      description              = "Allow port 9000"
+      cidr_blocks              = ["0.0.0.0/0"]
+    }
+    allow_port_8080 = {
+      type                     = "ingress"
+      from_port                = 8080
+      to_port                  = 8080
+      protocol                 = "tcp"
+      description              = "Allow port 8080"
+      cidr_blocks              = ["0.0.0.0/0"]
+    }
+    allow_port_3000 = {
+      type                     = "ingress"
+      from_port                = 3000
+      to_port                  = 3000
+      protocol                 = "tcp"
+      description              = "Allow port 3000"
+      cidr_blocks              = ["0.0.0.0/0"]
+    }
+  }
   tags = local.tags
 
 
