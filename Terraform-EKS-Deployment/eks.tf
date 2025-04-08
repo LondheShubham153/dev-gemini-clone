aab3c5f8ed7f2f1f534ae898647544bb8bb908bd
Amitabh-DevOps <amitabhdevops2024@gmail.com>
2025-04-08 18:11:25 +0530

    Increased volume size of nodes

--- Terraform-EKS-Deployment/eks.tf
+++ Terraform-EKS-Deployment/eks.tf
@@ -26,7 +26,7 @@ module "eks" {
 
   eks_managed_node_group_defaults = {
 
-    instance_types = ["t2.medium"]
+    instance_types = ["t2.large"]
 
     attach_cluster_primary_security_group = true
 
@@ -40,10 +40,10 @@ module "eks" {
       max_size     = 3
       desired_size = 2
 
-      instance_types = ["t2.medium"]
+      instance_types = ["t2.large"]
       capacity_type  = "SPOT"
 
-      disk_size = 30 
+      disk_size = 35 
       use_custom_launch_template = false  # Important to apply disk size!
 
       tags = {
